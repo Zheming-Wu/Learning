5cd1f5f217d164f8e1c4f350218e8503bc1b0a25
Wu Zheming <60833525+Zheming-Wu@users.noreply.github.com>
2024-04-18 11:33:55 +0300

    Add Process time monitoring function - Wu Zheming 2024/04/18

--- Professional_Study/Software_Engineering_MAI/Course_Project/Code/my_tele_bot.py
+++ Professional_Study/Software_Engineering_MAI/Course_Project/Code/my_tele_bot.py
@@ -1,5 +1,6 @@
 # function part
 import psutil
+import time
 
 
 def __proc_exist(process_name):
@@ -9,29 +10,60 @@ def __proc_exist(process_name):
             return pid
 
 
-def __terminate_process(pid):
+def __proc_terminate(pid):
     try:
         process = psutil.Process(pid)
         process.terminate()
-        process.wait()  # 等待进程退出
+        process.wait()  # Waiting for Process exit
         return True
     except psutil.NoSuchProcess:
         return False
 
 
+def __proc_time(pid):
+    for proc in psutil.process_iter(['pid', 'create_time']):
+        if proc.info['pid'] == pid:
+            pid_running_time = time.time() - proc.info['create_time']
+            return pid_running_time
+    pass
+
+
 # telegram part
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-my_token = ''
+with open("my_token.env", "r") as f:
+    data = f.read()
+    my_token = data.split('=')[1]
 
 bot = telebot.TeleBot(my_token, parse_mode='MARKDOWN')  # You can set parse_mode by default. HTML or MARKDOWN
 
 
+# Set up command bars
+bot.delete_my_commands(scope=None, language_code=None)
+bot.set_my_commands(
+    commands=[
+        telebot.types.BotCommand("/start", "Start Bot"),
+        telebot.types.BotCommand("/help", "Help with Bot"),
+        telebot.types.BotCommand("/check", "Check Process"),
+        telebot.types.BotCommand("/terminate", "Terminate Process"),
+    ],
+)
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello, which Process do you want to check?")
-    bot.register_next_step_handler(message, check_process)
+    chat_id = message.chat.id
+    bot.send_message(chat_id, text='Hello, what do you want to do on Process?')
+
+    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2)
+    button1 = telebot.types.KeyboardButton('/check')
+    button2 = telebot.types.KeyboardButton('/terminate')
+    keyboard.row(button1, button2)
+
+    bot.send_message(chat_id, 'Check or Terminate?', reply_markup=keyboard)
+
+    # bot.reply_to(message, "Hello, which Process do you want to check?")
+    # bot.register_next_step_handler(message, check_process)
 
 
 @bot.message_handler(commands=['check'])
@@ -42,8 +74,10 @@ def send_check_process(message):
 
 def check_process(message):
     process_name = message.text
-    if isinstance(__proc_exist(process_name), int):
-        bot.reply_to(message, f'{process_name} is running')
+    process_pid = __proc_exist(process_name)
+    process_time = __proc_time(process_pid)
+    if isinstance(process_pid, int):
+        bot.reply_to(message, f'{process_name} is running for {process_time} seconds.')
     else:
         bot.reply_to(message, 'no such process...')
 
@@ -57,15 +91,14 @@ def send_terminate_process(message):
 def terminate_process(message):
     process_name = message.text
     target_pid = __proc_exist(process_name)
-    target_process_name = process_name
 
     if target_pid:
-        if __terminate_process(target_pid):
-            bot.reply_to(message, f"Successfully terminated process PID: {str(target_pid)}")
+        if __proc_terminate(target_pid):
+            bot.reply_to(message, f"Successfully terminated Process {process_name} PID: {str(target_pid)}")
         else:
-            bot.reply_to(message, f"Failed to terminate process PID: {str(target_pid)}")
+            bot.reply_to(message, f"Failed to terminate Process {process_name} PID: {str(target_pid)}")
     else:
-        bot.reply_to(message, f"Target process {target_process_name} not found.")
+        bot.reply_to(message, f"Target process {process_name} not found.")
     pass
 
 
